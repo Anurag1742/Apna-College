af41044cb6af4a0edacbf897ab7feef37e9581cb
Anurag1742 <anuragranjan373@gmail.com>
2025-03-04 16:08:30 +0530

    Update J01StringNext.java

--- Basics/J01StringNext.java
+++ Basics/J01StringNext.java
@@ -1,8 +1,11 @@
+package Basics;
+
 import java.util.*;
 
-public class J01StringNext{
+public class J01StringNext {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String input=sc.next();
+        System.out.println(input);
     }
 }
