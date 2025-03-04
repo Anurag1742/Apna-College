f168020573f4a74f035b2095de0e4790c1f5ee95
Anurag1742 <anuragranjan373@gmail.com>
2025-03-04 17:49:04 +0530

    Update J01StringNext.java

--- Basics/J01StringNext.java
+++ Basics/J01StringNext.java
@@ -5,7 +5,16 @@ import java.util.*;
 public class J01StringNext {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String input=sc.next();
-        System.out.println(input);
+        // String input=sc.next();
+        // System.out.print(input);
+
+        // String name = sc.nextLine();
+        // System.out.print(name);
+
+        int number=sc.nextInt();
+        System.out.println(number);
+
+        Float price = sc.nextFloat();
+        System.out.println(price);
     }
 }
